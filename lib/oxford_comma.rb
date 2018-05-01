8740ddef102a442e32e745a7c50b08c4d85629f1
Dmitri Mendis <2dmendis@gmail.com>
2018-05-01 22:50:20 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,16 @@
 def oxford_comma(array)
-  
+  if array.size == 1 
+    new_string = array.join
+    elsif array.size == 2 
+    last = array.last
+    array.pop
+    new_string = array << " and "
+    new_string = array << last
+  else 
   last = array.last 
   array.pop
   
   new_string = array.join(", ")
   new_string << ", and #{last}"
+end 
 end
